eff413f512600c3255eb6b9a7a518df40187df1e
Mervius <mervius68@gmail.com>
2023-03-28 07:04:33 -0700

    sqlite using join successfully

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 
-
-
-const db = new sqlite3.Database("./combat.db", sqlite3.OPEN_READWRITE, (err) => {
-    if (err) return console.error(err.message);
-})
-
-
+const db = new sqlite3.Database(
+    "./combat.db",
+    sqlite3.OPEN_READWRITE,
+    (err) => {
+        if (err) return console.error(err.message);
+    }
+);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -21,17 +21,25 @@ app.get("/", function (req, res) {
 });
 
 app.get("/current_encounter/", (req, res) => {
-    console.log("tork")
-    let sql = `SELECT * FROM ct_tbl_actions`;
+    let sql = `SELECT *
+                FROM ct_tbl_campaigns
+                JOIN ct_tbl_encounters
+                ON ct_tbl_campaigns.cID = ct_tbl_encounters.cID
+                WHERE ct_tbl_campaigns.cID = 1;
+    `;
+            // above explicity join is basically the same as the following IMPLICIT join
+            // SELECT * FROM ct_tbl_campaigns, ct_tbl_encounters 
+            // where ct_tbl_campaigns.cID = ct_tbl_encounters.cID
+            // AND ct_tbl_campaigns.cID = 1
+
     let query = db.all(sql, [], (err, results) => {
         if (err) {
-            console.log("yikes")
             throw err;
         }
-        console.log(results)
+        console.log(results);
         res.send(results);
     });
-})
+});
 
 // app.get("/getEntry/:dateday", (req, res) => {
 //     let sql = `SELECT * FROM main where dateID ='${req.params.dateday}'`;
@@ -47,5 +55,4 @@ app.get("/current_encounter/", (req, res) => {
 //     });
 // });
 
-
-app.listen(3000, console.log('App Listening to port 3000'));
+app.listen(3000, console.log("App Listening to port 3000"));
