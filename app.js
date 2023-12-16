9f1a88c1ac494e2618fdbc39a4a98fe0697628f9
John Murray <jmurray@benaroyaresearch.org>
2023-12-15 17:03:24 -0800

    participant order uses secondary_init field now too when loading page, but init modal needs to be updated for such

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ app.get("/participants/:encounter", (req, res) => {
     let sql = `SELECT *
                 FROM ct_tbl_participant
                 JOIN tbl_character ON ct_tbl_participant.chID = tbl_character.chID
-                WHERE ct_tbl_participant.eID = ${encounter} ORDER BY init DESC, character_name, numeric_value ASC;
+                WHERE ct_tbl_participant.eID = ${encounter} ORDER BY init DESC, secondary_init DESC, character_name, numeric_value ASC;
                 `;
     let query = db.all(sql, [], (err, results) => {
         if (err) {
